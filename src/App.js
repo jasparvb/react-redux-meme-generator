21c7f49f54e64988ee7374b277fd1793b983e1a9
von Buelow <jaspar@UMICH.EDU>
2020-08-04 18:27:04 -0400

    DeleteMeme fn

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
 import React from 'react';
 import './App.css';
-import { useSelector } from "react-redux";
+import Meme from './Meme';
+import AddMemeForm from './AddMemeForm';
+import { useSelector, useDispatch } from "react-redux";
 
 function App() {
-const memes = useSelector(store => store.memes);
+  const memes = useSelector(store => store.memes);
+  const dispatch = useDispatch();
 
+  function deleteMeme(id) {
+    dispatch({type: "DELETE_MEME", id });
+  }
 
-
-const memeList = () => (
-  memes.map((meme, i) => <Meme 
-      key={i} 
-      id={i}
+  const memeList = memes.map(meme => (
+    <Meme 
+      key={meme.id} 
+      id={meme.id}
       textTop={meme.textTop}
       textBottom={meme.textBottom}
       img_url={meme.img_url}
       deleteMeme={deleteMeme} 
     />
-  )
-);
+  ));
 
   return (
     <div className="App">
-      <h1>Meme Generator</h1>
-      <ul className="App-meme-container">
-      {memeList}
-      </ul>
+      <div className="App-header">
+        <h1>Meme Generator</h1>
+        <AddMemeForm />
+      </div>
+      <div className="App-main">
+        <ul className="App-meme-container">
+        {memeList}
+        </ul>
+      </div>
     </div>
   );
 }
